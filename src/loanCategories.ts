06c9e9d42587f92399a706251d5a0379df9fcb62
Todd Rowe <todd.rowe@bitxfunding.com>
2026-02-21 20:33:25 -0500

    Create loanCategories module

---
+++ src/loanCategories.ts
@@ -0,0 +1,9 @@
+// loanCategories.ts
+
+export const loanCategories = [
+    { id: 1, name: 'Home Loan' },
+    { id: 2, name: 'Personal Loan' },
+    { id: 3, name: 'Auto Loan' },
+    { id: 4, name: 'Student Loan' },
+    { id: 5, name: 'Business Loan' }
+];
